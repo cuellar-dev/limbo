001d061a46d6154e86be1948a185c8fe06069661
Luis <luisernesto.cuellar164@gmail.com>
2026-04-27 00:23:38 -0400

    estilo pildora nice

---
+++ js/js.js
@@ -0,0 +1,38 @@
+const switchContainer = document.querySelector('.switch-container');
+
+if (switchContainer) {
+	const switchIndicator = switchContainer.querySelector('.color-switch');
+	const switchButtons = Array.from(switchContainer.querySelectorAll('button'));
+
+	const setActiveSwitch = (button) => {
+		if (!switchIndicator || !button) return;
+
+		switchButtons.forEach((btn) => btn.classList.remove('is-active'));
+		button.classList.add('is-active');
+		switchContainer.classList.toggle('is-encargue', switchButtons.indexOf(button) === 1);
+
+		const containerRect = switchContainer.getBoundingClientRect();
+		const buttonRect = button.getBoundingClientRect();
+		const buttonCenterX = (buttonRect.left - containerRect.left) + (buttonRect.width / 2);
+		const indicatorX = buttonCenterX - (buttonRect.width / 2);
+
+		switchIndicator.style.width = `${buttonRect.width}px`;
+		switchIndicator.style.transform = `translate(${indicatorX}px, -50%)`;
+	};
+
+	switchButtons.forEach((button) => {
+		button.addEventListener('click', () => setActiveSwitch(button));
+	});
+
+	// Estado inicial: primer boton activo
+	if (switchButtons.length > 0) {
+		setActiveSwitch(switchButtons[0]);
+	}
+
+	window.addEventListener('resize', () => {
+		const activeButton = switchContainer.querySelector('button.is-active') || switchButtons[0];
+		if (activeButton) {
+			setActiveSwitch(activeButton);
+		}
+	});
+}
